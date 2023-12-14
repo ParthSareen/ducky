f28162d5e65027b1860d0311b631c6e285ab5ee8
ParthSareen <psareen@uwaterloo.ca>
2023-12-14 13:57:54 -0500

    "Added Ollama model and TODO for enabling it, removed prompt input and print statement."

--- ducky/ducky.py
+++ ducky/ducky.py
@@ -20,18 +20,18 @@ def call_llama(code: str, prompt: Optional[str] = None):
         prompt = prompt + code
     system_prompt = "You are a pair progamming tool to help developers debug, think through design, and write code. Help the user rubber duck by providing feedback on the code."
 
-    llm = Ollama(model="codellama:7b-python", callbacks=[StreamingStdOutCallbackHandler()], system=system_prompt)
-    # llm(prompt)
+    # TODO: find out how to enable the python trained model
+    llm = Ollama(model="codellama", callbacks=[StreamingStdOutCallbackHandler()], system=system_prompt)
+
+    # TODO: add chaining if it makes sense
     llm(prompt)
-    print('finished')
 
 def ducky():
     parser = argparse.ArgumentParser()
     parser.add_argument("--prompt", help="Custom prompt to be used", default=None)
     parser.add_argument("--file", help="The file to be processed", default=None)
     parser.add_argument("--directory", help="The directory to be processed", default=None)
-    args, unknown = parser.parse_known_args()
-
+    args = parser.parse_known_args()
 
     if args.file is not None:
         code = open(args.file).read()
