cf080d98ad76a23a92a262d59a0a6e84c8559b6f
ParthSareen <psareen@uwaterloo.ca>
2023-12-14 14:05:57 -0500

    Cleanup

--- ducky/ducky.py
+++ ducky/ducky.py
@@ -1,24 +1,22 @@
 import argparse
 from typing import Optional
 from langchain.llms.ollama import Ollama
-from langchain.document_loaders import ObsidianLoader 
-from langchain.embeddings import OllamaEmbeddings
-from langchain.schema import StrOutputParser
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import chroma
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chains import RetrievalQA
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-# from transformers import LlamaForCausalLM, CodeLlamaTokenizer
 
 
-def call_llama(code: str, prompt: Optional[str] = None):
+def call_llama(code: str, prompt: Optional[str] = None) -> None:
+    """
+    This function calls the Ollama model to provide feedback on the given code.
+
+    Args:
+        code (str): The code to be reviewed.
+        prompt (Optional[str], optional): Custom prompt to be used. Defaults to None.
+    """
     if prompt is None:
         prompt = "review the code, find any issues if any, suggest cleanups if any:" + code
     else:
         prompt = prompt + code
-    system_prompt = "You are a pair progamming tool to help developers debug, think through design, and write code. Help the user rubber duck by providing feedback on the code."
+    system_prompt = """You are a pair progamming tool to help developers debug, think through design, and write code. Help the user rubber duck by providing feedback on the code."""
 
     # TODO: find out how to enable the python trained model
     llm = Ollama(model="codellama", callbacks=[StreamingStdOutCallbackHandler()], system=system_prompt)
@@ -26,12 +24,15 @@ def call_llama(code: str, prompt: Optional[str] = None):
     # TODO: add chaining if it makes sense
     llm(prompt)
 
-def ducky():
+def ducky() -> None:
+    """
+    This function parses the command line arguments and calls the Ollama model.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--prompt", help="Custom prompt to be used", default=None)
     parser.add_argument("--file", help="The file to be processed", default=None)
     parser.add_argument("--directory", help="The directory to be processed", default=None)
-    args = parser.parse_known_args()
+    args, _ = parser.parse_known_args()
 
     if args.file is not None:
         code = open(args.file).read()
